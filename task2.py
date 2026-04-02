ac14c546e4932316ecd69eee16e799cc4da8efe6
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2026-04-02 12:34:57 +0200

    task2

--- task2.py
+++ task2.py
@@ -1,98 +1,84 @@
-# Щоб виконати принцип єдиної відповідальності (SRP), створіть клас Book, 
-# який відповідатиме за зберігання інформації про книгу.
-# Щоб забезпечити принцип відкритості/закритості (OCP), зробіть так, 
-# щоб клас Library міг бути розширений для нової функціональності без зміни його коду.
-# Щоб виконати принцип підстанови Лісков (LSP), переконайтеся, що будь-який клас, 
-# який наслідує інтерфейс LibraryInterface, може замінити клас Library без порушення роботи програми.
-# Щоб виконати принцип розділення інтерфейсів (ISP), 
-# використовуйте інтерфейс LibraryInterface для чіткої специфікації методів, 
-# які необхідні для роботи з бібліотекою library.
-# Щоб виконати принцип інверсії залежностей (DIP), зробіть так, щоб класи вищого рівня, 
-# такі як LibraryManager, залежали від абстракцій (інтерфейсів), а не від конкретних реалізацій класів.
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
-class Library:
+
+@dataclass
+class Book:
+    title: str
+    author: str
+    year: int
+
+    def __str__(self):
+        return f"{self.title} by {self.author} {self.year}"
+
+
+class LibraryInterface(ABC):
+    @abstractmethod
+    def add_book(self, title, author, year): ...
+    @abstractmethod
+    def remove_book(self, title): ...
+    @abstractmethod
+    def show_books(self): ...
+
+
+class Library(LibraryInterface):
     def __init__(self):
-        self.books = []
+        self.books: list[Book] = []
 
     def add_book(self, title, author, year):
-        book = {
-            "title": title,
-            "author": author,
-            "year": year
-        }
-        self.books.append(book)
+
+        self.books.append(Book(title, author, int(year)))
 
     def remove_book(self, title):
         for book in self.books:
-            if book["title"] == title:
+
+            if book.title == title:
                 self.books.remove(book)
                 break
 
     def show_books(self):
         for book in self.books:
-            print(f'Title: {book["title"]}, Author: {book["author"]}, Year: {book["year"]}')
+            print(f"Title: {book.title}, Author: {book.author}, Year: {book.year}")
+
+
+class LibraryManager:
+    def __init__(self, library: LibraryInterface):
+        self.library = library
+
+    def add_book(self, title, author, year):
+        self.library.add_book(title, author, year)
+
+    def remove_book(self, title):
+        self.library.remove_book(title)
+
+    def show_books(self):
+        self.library.show_books()
+
 
 def main():
     library = Library()
-    
+    manager = LibraryManager(library)
+
     while True:
         command = input("Enter command (add, remove, show, exit): ").strip().lower()
-        
-        if command == "add":
-            title = input("Enter book title: ").strip()
-            author = input("Enter book author: ").strip()
-            year = input("Enter book year: ").strip()
-            library.add_book(title, author, year)
-        elif command == "remove":
-            title = input("Enter book title to remove: ").strip()
-            library.remove_book(title)
-        elif command == "show":
-            library.show_books()
-        elif command == "exit":
-            break
-        else:
-            print("Invalid command. Please try again.")
+
+        match command:
+            case "add":
+                title = input("Enter book title: ").strip()
+                author = input("Enter book author: ").strip()
+                year = input("Enter book year: ").strip()
+                manager.add_book(title, author, year)
+            case "remove":
+                title = input("Enter book title to remove: ").strip()
+                manager.remove_book(title)
+            case "show":
+                manager.show_books()
+            case "exit":
+                break
+            case _:
+                print("Invalid command. Please try again.")
+
 
 if __name__ == "__main__":
+
     main()
-
-
-# from abc import ABC, abstractmethod
-
-# class Book:
-#     pass
-
-# class LibraryInterface(ABC):
-#     pass
-
-# class Library(LibraryInterface):
-#     pass
-
-# class LibraryManager:
-#     pass
-
-# def main():
-#     library = Library()
-#     manager = LibraryManager(library)
-
-#     while True:
-#         command = input("Enter command (add, remove, show, exit): ").strip().lower()
-
-#         match command:
-#             case "add":
-#                 title = input("Enter book title: ").strip()
-#                 author = input("Enter book author: ").strip()
-#                 year = input("Enter book year: ").strip()
-#                 manager.add_book(title, author, year)
-#             case "remove":
-#                 title = input("Enter book title to remove: ").strip()
-#                 manager.remove_book(title)
-#             case "show":
-#                 manager.show_books()
-#             case "exit":
-#                 break
-#             case _:
-#                 print("Invalid command. Please try again.")
-
-# if __name__ == "__main__":
-#     main()
